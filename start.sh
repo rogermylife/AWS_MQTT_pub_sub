6a61469778710ffa872002ab7fdf7ede5c465e3c
Rogermylife <rogermylifeQ@gmail.com>
2017-06-08 15:35:39 +0000

    Ver1.0 remoter

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Check to see if root CA file exists, download if not
-if [ ! -f ./root-CA.crt ]; then
+if [ ! -f ./secret/root-CA.crt ]; then
   printf "\nDownloading AWS IoT Root CA certificate from Symantec...\n"
   curl https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem > root-CA.crt
 fi
@@ -18,4 +18,4 @@ fi
 
 # run pub/sub sample app using certificates downloaded in package
 printf "\nRunning pub/sub sample application...\n"
-python aws-iot-device-sdk-python/samples/basicPubSub/basicPubSub.py -e a9g5ud07hpcdn.iot.us-east-1.amazonaws.com -r root-CA.crt -c MyRaspberryPi.cert.pem -k MyRaspberryPi.private.key
+python aws-iot-device-sdk-python/samples/basicPubSub/basicPubSub.py -e a9g5ud07hpcdn.iot.us-east-1.amazonaws.com -r secret/root-CA.crt -c secret/MyRaspberryPi.cert.pem -k secret/MyRaspberryPi.private.key
